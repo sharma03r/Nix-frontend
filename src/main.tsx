d04cf6460be04722928c29c71b579f2f6d74a684
Rahul Sharma <sharmarahul8848@gmail.com>
2024-02-23 20:32:04 +0530

    Updated the backend URL

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/authContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "https://nix-server.vercel.app/api/v1";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
